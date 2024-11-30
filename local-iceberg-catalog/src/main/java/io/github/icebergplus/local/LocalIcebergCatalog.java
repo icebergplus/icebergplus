c3bdee4eb996c4331c3af5ed8cec51e444fc18e6
sullis <github@seansullivan.com>
2024-11-30 11:50:52 -0800

    add LocalIcebergCatalog.java

---
+++ local-iceberg-catalog/src/main/java/io/github/icebergplus/local/LocalIcebergCatalog.java
@@ -0,0 +1,167 @@
+package io.github.icebergplus.local;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+import org.apache.iceberg.CatalogProperties;
+import org.apache.iceberg.aws.AwsClientProperties;
+import org.apache.iceberg.aws.s3.S3FileIO;
+import org.apache.iceberg.aws.s3.S3FileIOProperties;
+import org.apache.iceberg.jdbc.JdbcCatalog;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.MinIOContainer;
+import org.apache.iceberg.catalog.Catalog;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.NoSuchBucketException;
+
+
+public class LocalIcebergCatalog {
+  private static final Region AWS_REGION = Region.US_EAST_1;
+  private final String s3BucketName = "test-bucket";
+  private final String warehouseLocation = "s3://" + s3BucketName + "/iceberg";
+  private File localDir;
+  private File minioDataDir;
+  private File h2Dir;
+  private MinIOContainer minio;
+  private Catalog catalog;
+  private final Map<String, String> extraCatalogProperties;
+  private final AtomicReference<Status> status = new AtomicReference<>(Status.STOPPED);
+
+  enum Status {
+      STOPPED,
+      STARTING,
+      STARTED
+  }
+
+  public LocalIcebergCatalog() {
+      this(createTempDirectory(), new HashMap<>());
+  }
+
+  public LocalIcebergCatalog(final Map<String, String> extraCatalogProps) {
+    this(createTempDirectory(), extraCatalogProps);
+  }
+
+  public LocalIcebergCatalog(final File localDir) {
+    this(localDir, new HashMap<>());
+  }
+
+  public LocalIcebergCatalog(final File localDir, final Map<String, String> extraCatalogProps) {
+    this.localDir = localDir;
+    this.localDir.mkdirs();
+    this.minioDataDir = new File(this.localDir, "minio-data");
+    this.minioDataDir.mkdirs();
+    this.h2Dir = new File(this.localDir, "h2db-data");
+    this.h2Dir.mkdirs();
+    this.extraCatalogProperties = extraCatalogProps;
+  }
+
+  private static File createTempDirectory() {
+    try {
+      return Files.createTempDirectory("iceberg-local").toFile();
+    } catch (IOException ex) {
+      throw new IllegalStateException("unable to create localDir");
+    }
+  }
+
+  public File getLocalDirectory() {
+    return localDir;
+  }
+
+  public String getS3BucketName() {
+    return s3BucketName;
+  }
+
+  public S3Client createS3Client() {
+    final URI uri = URI.create(minio.getS3URL());
+    return S3Client.builder()
+      .region(AWS_REGION)
+      .credentialsProvider(
+        StaticCredentialsProvider.create(
+            AwsBasicCredentials.create(minio.getUserName(), minio.getPassword())))
+      .applyMutation(mutator -> mutator.endpointOverride(uri))
+      .forcePathStyle(true) // OSX won't resolve subdomains
+      .build();
+  }
+
+  public void start() {
+    if (!this.status.compareAndSet(Status.STOPPED, Status.STARTING)) {
+      throw new IllegalStateException("Cannot start. status=" + this.status.get());
+    }
+
+    if (minio == null) {
+      minio = new MinIOContainer("minio/minio:latest");
+      minio.withFileSystemBind(minioDataDir.getAbsolutePath(), "/data",  BindMode.READ_WRITE);
+      minio.withEnv("MINIO_DOMAIN", "localhost");
+    }
+
+    minio.start();
+
+    try (S3Client s3 = createS3Client()) {
+      try {
+        s3.headBucket(builder -> builder.bucket(s3BucketName));
+      } catch (NoSuchBucketException ex) {
+        s3.createBucket(builder -> builder.bucket(s3BucketName));
+      }
+    }
+
+    Map<String, String> props = new HashMap<>();
+    props.put(CatalogProperties.FILE_IO_IMPL, S3FileIO.class.getName());
+    props.put(CatalogProperties.URI, this.getJdbcUrl());
+    props.put(CatalogProperties.WAREHOUSE_LOCATION, warehouseLocation);
+    props.put(S3FileIOProperties.ACCESS_KEY_ID, this.minio.getUserName());
+    props.put(S3FileIOProperties.SECRET_ACCESS_KEY, this.minio.getPassword());
+    props.put(S3FileIOProperties.PATH_STYLE_ACCESS, "true");
+    props.put(S3FileIOProperties.ENDPOINT, this.minio.getS3URL());
+    props.put(AwsClientProperties.CLIENT_REGION, AWS_REGION.id());
+    if (this.extraCatalogProperties != null) {
+      props.putAll(this.extraCatalogProperties);
+    }
+
+    JdbcCatalog jdbc = new JdbcCatalog();
+    jdbc.initialize("jdbccatalog", props);
+    this.catalog = jdbc;
+
+    if (!this.status.compareAndSet(Status.STARTING, Status.STARTED)) {
+      throw new IllegalStateException("unable to complete start()");
+    }
+  }
+
+  public void stop() {
+    if (isStopped()) {
+      return;
+    }
+
+    if (minio != null) {
+      minio.stop();
+    }
+    this.status.set(Status.STOPPED);
+  }
+
+  public boolean isStopped() {
+    return this.status.get() == Status.STOPPED;
+  }
+
+  public String getWarehouseLocation() {
+    return this.warehouseLocation;
+  }
+
+  public Catalog getCatalog() {
+    return catalog;
+  }
+
+  public String getJdbcUrl() {
+    return "jdbc:h2:" + this.h2Dir.getAbsolutePath() + ";DATABASE_TO_UPPER=FALSE";
+  }
+
+  @Override
+  public String toString() {
+    return this.getClass().getName() + " " + this.status;
+  }
+}
---
+++ local-iceberg-catalog/src/test/java/io/github/icebergplus/local/LocalIcebergCatalogTest.java
@@ -0,0 +1,143 @@
+package io.github.icebergplus.local;
+
+import java.io.File;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+import java.util.UUID;
+import org.apache.iceberg.AppendFiles;
+import org.apache.iceberg.DataFiles;
+import org.apache.iceberg.MetricsConfig;
+import org.apache.iceberg.PartitionSpec;
+import org.apache.iceberg.Schema;
+import org.apache.iceberg.Table;
+import org.apache.iceberg.aws.s3.S3FileIO;
+import org.apache.iceberg.catalog.Catalog;
+import org.apache.iceberg.catalog.Namespace;
+import org.apache.iceberg.catalog.TableIdentifier;
+import org.apache.iceberg.data.GenericRecord;
+import org.apache.iceberg.data.Record;
+import org.apache.iceberg.data.parquet.GenericParquetWriter;
+import org.apache.iceberg.io.DataWriter;
+import org.apache.iceberg.io.InputFile;
+import org.apache.iceberg.io.OutputFile;
+import org.apache.iceberg.parquet.Parquet;
+import org.apache.iceberg.types.Types;
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import static org.apache.iceberg.types.Types.NestedField.required;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LocalIcebergCatalogTest {
+
+  @Test
+  public void happyPath() throws Exception {
+
+    LocalIcebergCatalog localCatalog = new LocalIcebergCatalog();
+    assertThat(localCatalog.getLocalDirectory()).exists().isDirectory();
+
+    localCatalog.start();
+
+    final Namespace namespace = Namespace.of("mynamespace");
+    final TableIdentifier tableIdentifier = TableIdentifier.of(namespace, "mytable");
+
+    final var columns = List.of(
+        required(1, "text", Types.StringType.get()),
+        required(2, "count", Types.IntegerType.get()),
+        required(3, "amazing", Types.BooleanType.get()),
+        required(4, "event_timestamp", Types.TimestampType.withZone())
+        );
+
+    final Schema schema = new Schema(columns);
+
+    final PartitionSpec spec = PartitionSpec.builderFor(schema).build();
+
+    {
+      Catalog catalog = localCatalog.getCatalog();
+      Table t = catalog.createTable(tableIdentifier, schema);
+      Table loaded = catalog.loadTable(tableIdentifier);
+      assertThat(t).isNotNull();
+      assertThat(loaded).isNotNull();
+      assertThat(t.location())
+          .isEqualTo(loaded.location());
+      assertThat(t.schema().schemaId())
+          .isEqualTo(loaded.schema().schemaId());
+      assertThat(t.schema().sameSchema(loaded.schema()))
+          .isTrue();
+
+      OutputFile outputFile = loaded.io().newOutputFile(loaded.location() + "/foobar/" + UUID.randomUUID() + ".parquet");
+
+      DataWriter<Record> dataWriter =
+          Parquet.writeData(outputFile)
+              .schema(schema)
+              .createWriterFunc(GenericParquetWriter::buildWriter)
+              .overwrite()
+              .metricsConfig(MetricsConfig.forTable(loaded))
+              .withSpec(spec)
+              .build();
+
+      GenericRecord record = GenericRecord.create(schema);
+      record.setField("text", "Hello world");
+      record.setField("count", 555);
+      record.setField("amazing", Boolean.TRUE);
+      record.setField("event_timestamp", OffsetDateTime.of(2005, 12, 1, 0, 0, 0, 0, ZoneOffset.ofHours(5)));
+
+      dataWriter.write(record);
+      dataWriter.close();
+
+      AppendFiles appendFiles = loaded.newAppend();
+
+      InputFile inputFile = outputFile.toInputFile();
+      System.out.println("location: " + inputFile.location());
+
+      appendFiles.appendFile(DataFiles.builder(spec)
+          .withInputFile(inputFile)
+          .withRecordCount(1)
+          .build());
+      appendFiles.commit();
+    }
+
+    localCatalog.stop();
+    assertThat(localCatalog.isStopped()).isTrue();
+
+    for (int i = 0; i < 2; i++) {
+      localCatalog.start();
+      assertThat(localCatalog.isStopped()).isFalse();
+
+      File localDir = localCatalog.getLocalDirectory();
+      assertThat(localDir).exists().isDirectory();
+
+      {
+        Catalog catalog = localCatalog.getCatalog();
+        Table loaded = catalog.loadTable(tableIdentifier);
+        assertThat(loaded).isNotNull();
+        assertThat(loaded.io()).isInstanceOf(S3FileIO.class);
+        assertThat(loaded.location()).startsWith("s3://test-bucket/iceberg/mynamespace/mytable");
+      }
+
+      localCatalog.stop();
+
+      localCatalog = new LocalIcebergCatalog(localDir);
+      localCatalog.start();
+      {
+        Catalog catalog = localCatalog.getCatalog();
+        Table loaded = catalog.loadTable(tableIdentifier);
+        assertThat(loaded).isNotNull();
+        assertThat(loaded.io()).isInstanceOf(S3FileIO.class);
+        assertThat(loaded.location()).startsWith("s3://test-bucket/iceberg/mynamespace/mytable");
+        assertThat(loaded.schema().sameSchema(schema))
+            .isTrue();
+        try (S3Client s3 = localCatalog.createS3Client()) {
+          assertBucketExists(s3, localCatalog.getS3BucketName());
+        }
+      }
+      localCatalog.stop();
+    }
+  }
+
+  private static void assertBucketExists(S3Client s3Client, String bucketName) {
+    var response = s3Client.headBucket(req -> req.bucket(bucketName));
+    assertThat(response.sdkHttpResponse().isSuccessful()).isTrue();
+  }
+}
