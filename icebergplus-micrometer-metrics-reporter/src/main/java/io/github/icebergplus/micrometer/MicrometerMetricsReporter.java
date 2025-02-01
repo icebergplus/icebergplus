314c19fd8c433712d6102f0c71c3989fea7fc230
sullis <github@seansullivan.com>
2025-02-01 11:34:53 -0800

    MicrometerMetricsReporter

--- icebergplus-micrometer-metrics-reporter/src/main/java/io/github/icebergplus/micrometer/MicrometerMetricsReporter.java
+++ icebergplus-micrometer-metrics-reporter/src/main/java/io/github/icebergplus/micrometer/MicrometerMetricsReporter.java
@@ -1,8 +1,11 @@
 package io.github.icebergplus.micrometer;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import org.apache.iceberg.metrics.CommitReport;
+import org.apache.iceberg.metrics.CounterResult;
 import org.apache.iceberg.metrics.MetricsReport;
 import org.apache.iceberg.metrics.MetricsReporter;
+import org.apache.iceberg.metrics.ScanReport;
 
 
 public class MicrometerMetricsReporter implements MetricsReporter {
@@ -18,6 +21,21 @@ public class MicrometerMetricsReporter implements MetricsReporter {
 
   @Override
   public void report(MetricsReport report) {
-    // todo
+    if (report instanceof CommitReport commitReport) {
+      report("commitReport.addedDeleteFiles", commitReport.commitMetrics().addedDeleteFiles());
+      report("commitReport.addedDataFiles", commitReport.commitMetrics().addedDataFiles());
+    } else if (report instanceof ScanReport scanReport) {
+      report("scanReport.totalFileSizeInBytes", scanReport.scanMetrics().totalFileSizeInBytes());
+    } else {
+      throw new IllegalArgumentException("unknown report type");
+    }
+  }
+
+  private void report(String name, CounterResult counterResult) {
+    if (counterResult == null) {
+      return;
+    }
+    var counter = meterRegistry.counter(name);
+    counter.increment(counterResult.value());
   }
 }
--- icebergplus-micrometer-metrics-reporter/src/test/java/io/github/icebergplus/micrometer/MicrometerMetricsReporterTest.java
+++ icebergplus-micrometer-metrics-reporter/src/test/java/io/github/icebergplus/micrometer/MicrometerMetricsReporterTest.java
@@ -1,12 +1,76 @@
 package io.github.icebergplus.micrometer;
 
-import org.junit.jupiter.api.Test;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import java.util.stream.Stream;
+import org.apache.iceberg.expressions.Expressions;
+import org.apache.iceberg.metrics.ImmutableCommitMetricsResult;
+import org.apache.iceberg.metrics.ImmutableCommitReport;
+import org.apache.iceberg.metrics.ImmutableCounterResult;
+import org.apache.iceberg.metrics.ImmutableScanMetricsResult;
+import org.apache.iceberg.metrics.ImmutableScanReport;
 
+import org.apache.iceberg.metrics.MetricsContext;
+import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MicrometerMetricsReporterTest {
+
   @Test
-  void happyPath() {
-    assertThat(true).isTrue();
+  void scanReport_happyPath() {
+    var scanResult = ImmutableScanMetricsResult.builder()
+        .totalFileSizeInBytes(ImmutableCounterResult.builder().unit(MetricsContext.Unit.BYTES).value(5678).build())
+        .build();
+    var report = ImmutableScanReport.builder()
+        .tableName("fooTable")
+        .snapshotId(2345)
+        .schemaId(666)
+        .filter(Expressions.alwaysTrue())
+        .scanMetrics(scanResult)
+        .build();
+
+    var registry = new SimpleMeterRegistry();
+
+    try (var reporter = new MicrometerMetricsReporter(registry)) {
+      reporter.report(report);
+    }
+
+    assertThat(meterNames(registry))
+        .containsExactly("scanReport.totalFileSizeInBytes");
+  }
+
+  @Test
+  void commitReport_happyPath() {
+    var commitResult = ImmutableCommitMetricsResult.builder()
+        .addedDataFiles(ImmutableCounterResult.builder()
+            .unit(MetricsContext.Unit.COUNT)
+            .value(10)
+            .build())
+        .addedDeleteFiles(ImmutableCounterResult.builder()
+            .unit(MetricsContext.Unit.COUNT)
+            .value(20)
+            .build())
+        .build();
+    var report = ImmutableCommitReport.builder()
+        .tableName("tableFoobar")
+        .snapshotId(555)
+        .sequenceNumber(444)
+        .operation("helloOperation")
+        .commitMetrics(commitResult)
+        .build();
+    var registry = new SimpleMeterRegistry();
+    try (var reporter = new MicrometerMetricsReporter(registry)) {
+      reporter.report(report);
+    }
+
+    assertThat(meterNames(registry))
+        .containsExactly(
+            "commitReport.addedDeleteFiles",
+            "commitReport.addedDataFiles");
+  }
+
+  private Stream<String> meterNames(MeterRegistry registry) {
+    return registry.getMeters().stream()
+        .map(m -> m.getId().getName());
   }
 }
